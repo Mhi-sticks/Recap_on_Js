4459ee153c747ef015f06a10fd2ab1b072b54057
Daniel <adeosundaniel33@gmail.com>
2022-12-30 02:17:19 +0100

    created a readme file and also created two variables using var keyword

---
+++ Readme.md
@@ -0,0 +1,5 @@
+**RECAP ON JAVASCRIPT**
+
+- How to use strings and how to escape from double quotes with variable "mystr" created.
+- A var of summ is also created for addition of two numbers.
+- symbols used mostly in js are shown.
---
+++ recap.js
@@ -0,0 +1,20 @@
+// recap on js--------------------------
+
+var summ = 10 + 0;
+
+console.log(summ);
+
+// when you hav a string and you hav a quotation mark in the middle, you use backslash to escape it
+var mystr = 'i am a "double quoted" string inside "double quotes"';
+console.log(mystr);
+/***
+ code output
+ \' single quote
+ \" double quote
+ \\ backslash
+ \n newline
+ \r carriage return
+ \t tab
+ \b backspace
+ \f form feed
+ ***/
