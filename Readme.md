ddba01caaf48ec2fbda1da9a7dfc0ce6f0846d08
Daniel <adeosundaniel33@gmail.com>
2022-12-30 17:56:09 +0100

    functions, booleans added to the js file

--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@
 - A var of summ is also created for addition of two numbers.
 - Symbols used mostly in js are shown.
 - Added operators like shift, unshift, pop and push.
+- Functions, immitating queues, counting cards and booleans.
--- recap.js
+++ recap.js
@@ -38,3 +38,81 @@ console.log(shiftedArray);
 var shiftedArray = array.unshift("jefferey");
 console.log(array);
 console.log(shiftedArray);
+
+// function---------------------------
+// for function, local variables takes precedence over global varibles
+
+// immitating queue------------------------
+function nextInLine(arr, item) {
+    arr.push(item);
+    // return item-----------------;
+    return arr.shift();
+  }
+  
+  var testArr = [1, 2, 3, 4, 5];
+  console.log("Before:" + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After:" + JSON.stringify(testArr));
+  
+  // boolean-----------------------------------------
+  function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+      return "yes, it's true";
+    }
+    return "No, it's false";
+  }
+  
+  console.log(ourTrueOrFalse(true));
+  
+  function isLess(a, b) {
+    return a < b;
+  }
+  
+  console.log(isLess(2, 7));
+  
+  //returning early pattern from functions----------------
+  function abTest(a, b) {
+    if (a < 0 || b < 0) {
+      return undefined;
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  console.log(abTest(-2, 2));
+  
+  //counting cards----------------------
+  
+  var count = 0;
+  
+  function cc(card) {
+    switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        count++;
+        break;
+      case 10:
+      case "J":
+      case "Q":
+      case "K":
+      case "A":
+        count--;
+        break;
+    }
+  
+    var holdbet = "Hold";
+    if (count > 0) {
+      holdbet = "Bet";
+    }
+    return count + " " + holdbet;
+  }
+  
+  cc(2);
+  cc("K");
+  cc(10);
+  cc("k");
+  cc("A");
+  console.log(cc(4));
+  
